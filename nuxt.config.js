9070281f3dd22c88e63bdbaac0db6927119d94a2
jaaaackZheng <512400820@qq.com>
2020-12-18 17:05:19 +0800

    import vuetify

--- nuxt.config.js
+++ nuxt.config.js
@@ -14,10 +14,10 @@ export default {
   },
 
   // Global CSS (https://go.nuxtjs.dev/config-css)
-  css: ['ant-design-vue/dist/antd.css'],
+  css: ['vuetify/dist/vuetify.css'],
 
   // Plugins to run before rendering page (https://go.nuxtjs.dev/config-plugins)
-  plugins: ['@/plugins/antd-ui'],
+  plugins: ['@/plugins/vuetify'],
 
   // Auto import components (https://go.nuxtjs.dev/config-components)
   components: true,
@@ -25,6 +25,7 @@ export default {
   // Modules for dev and build (recommended) (https://go.nuxtjs.dev/config-modules)
   buildModules: [
     // https://go.nuxtjs.dev/typescript
+    '@nuxtjs/vuetify',
     '@nuxt/typescript-build',
   ],
 
